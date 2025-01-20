ad8e0024d0ca3b54b9dc876e84f3bea3db0d4473
Afzaal Isnaufal <70215760+afzaalis@users.noreply.github.com>
2025-01-20 09:05:02 +0700

    delete weatherForecast

--- WebApplication1/Models/Book.cs
+++ WebApplication1/Models/Book.cs
@@ -3,8 +3,8 @@
         public class Book
         {
             public int Id { get; set; }
-            public string Title { get; set; } = string.Empty;
-            public string Author { get; set; } = string.Empty;
+            public string Title { get; set; } 
+            public string Author { get; set; } 
             public double Price { get; set; }
         }
 
